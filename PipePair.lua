c61493a2026a90c379a0cbe28b9f3be1411fbe61
dlmbc <delambacaekang@gmail.com>
2020-12-13 16:34:34 +0800

    randomize the gap between pipes

--- PipePair.lua
+++ PipePair.lua
@@ -1,8 +1,5 @@
 PipePair = Class{}
 
--- size of the gap between pipes
-local GAP_HEIGHT = 90
-
 function PipePair:init(y)
     -- flag to hold whether this pair has been scored (jumped through)
     self.scored = false
@@ -16,7 +13,7 @@ function PipePair:init(y)
     -- instantiate two pipes that belong to this pair
     self.pipes = {
         ['upper'] = Pipe('top', self.y),
-        ['lower'] = Pipe('bottom', self.y + PIPE_HEIGHT + GAP_HEIGHT)
+        ['lower'] = Pipe('bottom', self.y + PIPE_HEIGHT + math.random(80, 120))
     }
 
     -- whether this pipe pair is ready to be removed from the scene
